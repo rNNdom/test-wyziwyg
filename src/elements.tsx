1067aaf54920a98b740132870e30392b5eeb1b46
rNNMdom <l.galeano01@ufromail.cl>
2024-09-26 13:29:40 -0300

    removed elements.tsx

--- src/elements.tsx
+++
@@ -1,8 +0,0 @@
-import Button from "./components/Button";
-
-const children = "Click me"; // Define the children variable
-
-export const elements = {
-  id: 1, name: 'Button', children: <Button>{children}</Button>,
-
-}
